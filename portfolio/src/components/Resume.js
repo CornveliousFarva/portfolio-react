5c3e85680287f6614f7a60aff7e34d832458334f
Greg Schuman <gcschuman@hotmail.com>
2020-11-23 20:29:32 -0800

    Added the basic layout for the resume page.

--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -1,4 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import Footer from '/footer'
 
-
+const Resume = (props) => {
+  return (
+    <Container fluid>
+      <Row xs="2">
+        <Col>Column</Col>
+        <Col>Column</Col>
+        <Col>Column</Col>
+        <Col>Column</Col>
+      </Row>
+      <Footer />
+      </Container>
+  )
+}
 export default Resume;
