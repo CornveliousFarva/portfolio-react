2a50296e8b652900902d9aaa01215532d495daa3
Greg Schuman <gcschuman@hotmail.com>
2020-11-24 20:26:01 -0800

    Added a card, which will be the basic structure of projects that will be added.

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,23 +1,111 @@
-import React from 'react';
-import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button
-} from 'reactstrap';
+import React, { useState } from 'react';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col,  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button } from 'reactstrap';
+import classnames from 'classnames';
+import Footer from './footer'
 
 const Example = (props) => {
+  const [activeTab, setActiveTab] = useState('1');
+
+  const toggle = tab => {
+    if(activeTab !== tab) setActiveTab(tab);
+  }
+
   return (
     <div>
-      <Card>
-        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-        <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
+      <Nav tabs>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === '1' })}
+            onClick={() => { toggle('1'); }}
+          >
+            JavaScript
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === '2' })}
+            onClick={() => { toggle('2'); }}
+          >
+            React
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === '3' })}
+            onClick={() => { toggle('3'); }}
+          >
+            Python
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <TabContent activeTab={activeTab}>
+        <TabPane tabId="1">
+          <Row>
+            <Col sm="12">
+              <h4>JavaScript</h4>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+            <Card>
+                <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+                <CardBody>
+                  <CardTitle tag="h5">Index Cards</CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+                  <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                  <Button color="primary">Project</Button>
+                  <Button color="secondary">GitHub</Button>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col></Col>
+            <Col></Col>
+          </Row>
+          <Row>
+            <Col></Col>
+            <Col></Col>
+            <Col></Col>
+          </Row>
+        </TabPane>
+        <TabPane tabId="2">
+        <Row>
+            <Col sm="12">
+              <h4>React</h4>
+            </Col>
+          </Row>
+          <Row>
+            <Col></Col>
+            <Col></Col>
+            <Col></Col>
+          </Row>
+          <Row>
+            <Col></Col>
+            <Col></Col>
+            <Col></Col>
+          </Row>
+        </TabPane>
+        <TabPane tabId="3">
+        <Row>
+            <Col sm="12">
+              <h4>Python</h4>
+            </Col>
+          </Row>
+          <Row>
+            <Col></Col>
+            <Col></Col>
+            <Col></Col>
+          </Row>
+          <Row>
+            <Col></Col>
+            <Col></Col>
+            <Col></Col>
+          </Row>
+        </TabPane>
+      </TabContent>
+      <Footer />
     </div>
   );
-};
+}
 
 export default Example;
