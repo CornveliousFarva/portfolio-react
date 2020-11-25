56864ee22d3262366b91fac29e7b7f3b58c0676d
Greg Schuman <gcschuman@hotmail.com>
2020-11-24 20:05:47 -0800

    Updated links in the footer.

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -3,8 +3,7 @@ import { Container, Row, Col } from 'reactstrap';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
     faGithub,
-    faLinkedin,
-    
+    faLinkedin,   
 } from "@fortawesome/free-brands-svg-icons";
 import { faBlog, faEnvelope, } from '@fortawesome/free-solid-svg-icons'
 
@@ -12,8 +11,8 @@ function Footer(){
     return(
         <Container fluid>
             <Row>
-                <Col><FontAwesomeIcon icon={faGithub} size="3x" href="https://github.com/CornveliousFarva"></FontAwesomeIcon></Col>
-                <Col><FontAwesomeIcon icon={faLinkedin} size="3x" href="https://www.linkedin.com/in/greg-schuman/"></FontAwesomeIcon></Col>
+                <Col><FontAwesomeIcon icon={faGithub} size="3x"><link a href="https://github.com/CornveliousFarva" /></FontAwesomeIcon></Col>
+                <Col><FontAwesomeIcon icon={faLinkedin} size="3x"><link a href="https://www.linkedin.com/in/greg-schuman/" /></FontAwesomeIcon></Col>
                 <Col><FontAwesomeIcon icon={faEnvelope} size="3x"></FontAwesomeIcon>greg.c.schuman@gmail.com </Col>
                 <Col><FontAwesomeIcon icon={faBlog} size="3x"></FontAwesomeIcon></Col>
             </Row>
