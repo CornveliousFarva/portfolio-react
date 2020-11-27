31eeb3eefbc77cc7f2851e71d5a4601afa1aa0e9
Greg Schuman <gcschuman@hotmail.com>
2020-11-27 10:59:59 -0800

    Changed the card styling from tabs to a single page of cards and added project links.

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,115 +1,51 @@
-import React, { useState } from 'react';
-import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col,  Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button } from 'reactstrap';
-import classnames from 'classnames';
-import Footer from './footer'
+import React from 'react';
+import {
+  Card, Button, CardImg, CardTitle, CardText, CardGroup,
+  CardSubtitle, CardBody
+} from 'reactstrap';
 
-const Example = (props) => {
-  const [activeTab, setActiveTab] = useState('1');
 
-  const toggle = tab => {
-    if(activeTab !== tab) setActiveTab(tab);
-  }
-
+const Projects = (props) => {
   return (
-    <div>
-      <Nav tabs>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === '1' })}
-            onClick={() => { toggle('1'); }}
-          >
-            JavaScript
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === '2' })}
-            onClick={() => { toggle('2'); }}
-          >
-            React
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === '3' })}
-            onClick={() => { toggle('3'); }}
-          >
-            Python
-          </NavLink>
-        </NavItem>
-      </Nav>
-      <TabContent activeTab={activeTab}>
-        <TabPane tabId="1">
-          <Row>
-            <Col sm="12">
-              <h4>JavaScript</h4>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-            <Card>
-                <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-                <CardBody>
-                  <CardTitle tag="h5">Index Cards</CardTitle>
-                  <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                  <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                  <Button color="primary">Project</Button>
-                  <Button color="secondary">GitHub</Button>
+    <CardGroup>
+       <Card>
+        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+          <CardBody>
+            <CardTitle tag="h5">Index Cards</CardTitle>
+              <CardSubtitle tag="h6" className="mb-2 text-muted">HTML, CSS, and JavaScript.</CardSubtitle>
+                  <CardText>Quiz cards designed to test the user's knowledge on a subject or subjects</CardText>
+                  <a href="https://cornveliousfarva.github.io/quiz-cards/"><Button color="primary">Project</Button></a>
+                  <a href="https://github.com/CornveliousFarva/quiz-cards"><Button color="secondary">GitHub</Button></a>
                 </CardBody>
               </Card>
-            </Col>
-            <Col>
-            
-            </Col>
-            <Col>
-            
-            </Col>
-          </Row>
-          <Row>
-            <Col></Col>
-            <Col></Col>
-            <Col></Col>
-          </Row>
-        </TabPane>
-        <TabPane tabId="2">
-        <Row>
-            <Col sm="12">
-              <h4>React</h4>
-            </Col>
-          </Row>
-          <Row>
-            <Col></Col>
-            <Col></Col>
-            <Col></Col>
-          </Row>
-          <Row>
-            <Col></Col>
-            <Col></Col>
-            <Col></Col>
-          </Row>
-        </TabPane>
-        <TabPane tabId="3">
-        <Row>
-            <Col sm="12">
-              <h4>Python</h4>
-            </Col>
-          </Row>
-          <Row>
-            <Col></Col>
-            <Col></Col>
-            <Col></Col>
-          </Row>
-          <Row>
-            <Col></Col>
-            <Col></Col>
-            <Col></Col>
-          </Row>
-        </TabPane>
-      </TabContent>
-      <Footer />
-    </div>
+      <Card>
+      <Card>
+        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+          <CardBody>
+            <CardTitle tag="h5">Password Generator</CardTitle>
+              <CardSubtitle tag="h6" className="mb-2 text-muted">HTML, CSS, and JavaScript.</CardSubtitle>
+                <CardText>Password generator for security of personal and professional accounts.</CardText>
+                  <a href="https://cornveliousfarva.github.io/password-generator/"><Button color="primary">Project</Button></a>
+                  <a href="https://github.com/CornveliousFarva/password-generator"><Button color="secondary">GitHub</Button></a>
+                </CardBody>
+              </Card>
+      </Card>
+      <Card>
+        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+          <CardBody>
+            <CardTitle tag="h5">Official Livepool FC Supporters Club-Carlsbad, California</CardTitle>
+              <CardSubtitle tag="h6" className="mb-2 text-muted">React JS and Reactsrap.</CardSubtitle>
+                <CardText>Website for a soccer team's supporters group designed to give it more visibility.</CardText>
+                  <a href="https://carlsbad-kopites.netlify.app"><Button color="primary">Project</Button></a>
+                  <a href="https://github.com/CornveliousFarva/olsc-carlsbad "><Button color="secondary">GitHub</Button></a>
+                </CardBody>
+      </Card>
+      <Card>
+        
+      </Card>
+    </CardGroup>
   );
-}
+};
 
-export default Example;
+export default Projects;
+            
