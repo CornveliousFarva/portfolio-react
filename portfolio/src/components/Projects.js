24d003fb9825a4a598faa9142928aa1fab64ff50
Greg Schuman <gcschuman@hotmail.com>
2020-11-27 11:29:50 -0800

    Overhauled the organization of the projects page, making it so that way only three projects were on a row. And I added the footer.

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,56 +1,85 @@
 import React from 'react';
 import {
-  Card, Button, CardImg, CardTitle, CardText, CardGroup,
-  CardSubtitle, CardBody
+  Card, Button, CardImg, CardTitle, CardText,
+  CardSubtitle, CardBody, Container, Row, Col
 } from 'reactstrap';
+import Footer from './footer'
 
 
 const Projects = (props) => {
   return (
-    <CardGroup>
-       <Card>
-        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-          <CardBody>
-            <CardTitle tag="h5">Index Cards</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">HTML, CSS, and JavaScript.</CardSubtitle>
+    <Container>
+      <Row>
+        <Col>
+          <Card>
+            <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+            <CardBody>
+              <CardTitle tag="h5">Index Cards</CardTitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">HTML, CSS, and JavaScript.</CardSubtitle>
                   <CardText>Quiz cards designed to test the user's knowledge on a subject or subjects</CardText>
                   <a href="https://cornveliousfarva.github.io/quiz-cards/"><Button color="primary">Project</Button></a>
                   <a href="https://github.com/CornveliousFarva/quiz-cards"><Button color="secondary">GitHub</Button></a>
                 </CardBody>
               </Card>
-      <Card>
-      <Card>
-        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-          <CardBody>
-            <CardTitle tag="h5">Password Generator</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">HTML, CSS, and JavaScript.</CardSubtitle>
-                <CardText>Password generator for security of personal and professional accounts.</CardText>
-                  <a href="https://cornveliousfarva.github.io/password-generator/"><Button color="primary">Project</Button></a>
-                  <a href="https://github.com/CornveliousFarva/password-generator"><Button color="secondary">GitHub</Button></a>
+        </Col>
+        <Col>
+          <Card>
+            <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+              <CardBody>
+                <CardTitle tag="h5">Password Generator</CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">HTML, CSS, and JavaScript.</CardSubtitle>
+                    <CardText>Password generator for security of personal and professional accounts.</CardText>
+                      <a href="https://cornveliousfarva.github.io/password-generator/"><Button color="primary">Project</Button></a>
+                      <a href="https://github.com/CornveliousFarva/password-generator"><Button color="secondary">GitHub</Button></a>
                 </CardBody>
               </Card>
-      </Card>
-      <Card>
-        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-          <CardBody>
-            <CardTitle tag="h5">Official Livepool FC Supporters Club-Carlsbad, California</CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">React JS and Reactsrap.</CardSubtitle>
-                <CardText>Website for a soccer team's supporters group designed to give it more visibility.</CardText>
-                  <a href="https://carlsbad-kopites.netlify.app"><Button color="primary">Project</Button></a>
-                  <a href="https://github.com/CornveliousFarva/olsc-carlsbad "><Button color="secondary">GitHub</Button></a>
+        </Col>
+        <Col>
+          <Card>
+            <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+              <CardBody>
+                  <CardTitle tag="h5">Official Livepool FC Supporters Club-Carlsbad, California</CardTitle>
+                    <CardSubtitle tag="h6" className="mb-2 text-muted">React JS and Reactsrap.</CardSubtitle>
+                      <CardText>Website for a soccer team's supporters group designed to give it more visibility.</CardText>
+                        <a href="https://carlsbad-kopites.netlify.app"><Button color="primary">Project</Button></a>
+                        <a href="https://github.com/CornveliousFarva/olsc-carlsbad "><Button color="secondary">GitHub</Button></a>
                 </CardBody>
       </Card>
-      <Card>
-        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-          <CardBody>
-            <CardTitle tag="h5">Quote Generator </CardTitle>
-              <CardSubtitle tag="h6" className="mb-2 text-muted">React</CardSubtitle>
-                <CardText>Quote generator with buttons to share to social media</CardText>
-                  <a href="https://cornveliousfarva.github.io/quote-generator/"><Button color="primary">Project</Button></a>
-                  <a href="https://github.com/CornveliousFarva/quote-generator"><Button color="secondary">GitHub</Button></a>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Card>
+            <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+              <CardBody>
+                <CardTitle tag="h5">Quote Generator </CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">React</CardSubtitle>
+                    <CardText>Quote generator with buttons to share to social media</CardText>
+                      <a href="https://cornveliousfarva.github.io/quote-generator/"><Button color="primary">Project</Button></a>
+                      <a href="https://github.com/CornveliousFarva/quote-generator"><Button color="secondary">GitHub</Button></a>
                 </CardBody>
       </Card>
-    </CardGroup>
+        </Col>
+        <Col>
+        
+        </Col>
+        <Col>
+        
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+        
+        </Col>
+        <Col>
+        
+        </Col>
+        <Col>
+        
+        </Col>
+      </Row>
+      <Footer />
+    </Container>
   );
 };
 
